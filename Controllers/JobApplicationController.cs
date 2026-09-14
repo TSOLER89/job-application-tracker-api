aa588a45ecbbf55600f3a962f36866fc9ecfa2c1
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-09-14 14:22:25 +0200

    Lägg till GET-endpoint för jobbansökningar Skapat ASP.NET Web API projekt för job-application-tracker Lägg till modell för jobbansökningar Lägg till GET-endpoint för jobbansökningar

---
+++ Controllers/JobApplicationController.cs
@@ -0,0 +1,50 @@
+﻿using job_application_tracker_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace job_application_tracker_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JobApplicationsController : ControllerBase
+{
+    private static readonly List<JobApplication> Applications =
+    [
+        new JobApplication
+        {
+            Id = 1,
+            Company = "Consid",
+            Position = "Junior .NET-utvecklare",
+            Location = "Linköping",
+            DateApplied = new DateOnly(2026, 9, 1),
+            Status = "Intervju",
+            Notes = ""
+        },
+
+        new JobApplication
+        {
+            Id = 2,
+            Company = "Sectra",
+            Position = "Systemutvecklare",
+            Location = "Linköping",
+            DateApplied = new DateOnly(2026, 9, 9),
+            Status = "Ansökt",
+            Notes = ""
+        },
+
+        new JobApplication
+        {
+            Id = 3,
+            Company = "Saab",
+            Position = "Junior Software Developer",
+            Location = "Linköping",
+            Status = "Intresserad",
+            Notes = ""
+        }
+    ];
+
+    [HttpGet]
+    public ActionResult<List<JobApplication>> GetAll()
+    {
+        return Ok(Applications);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+//lägg till stöd för controllers
+builder.Services.AddControllers();
+
+//OpenAPI används för att beskriva API:et
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +16,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+//Gör våra controllers tillgängliga via endpoints
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
