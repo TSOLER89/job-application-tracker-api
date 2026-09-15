8371ea9b23b94483d3893665c78e77ac0165b640
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-09-15 10:00:36 +0200

    Konfigurera CORS för React-frontend

--- Program.cs
+++ Program.cs
@@ -9,6 +9,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//API:t tillåter vår React-app 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactApp", policy =>
+    {
+        policy
+              .WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
